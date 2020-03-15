0df4227904fe4e74a8b3a39a3af5d7ff7d24e58e
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-15 08:50:11 +0530

    added process.env.port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require('./lib/routers');
 
+const port = process.env.PORT || 4000;
+
 const main = function() {
-  app.listen(8000, () => console.log('server started listening on port 8000'));
+  app.listen(port, () =>
+    console.log(`server started listening on port ${port}`)
+  );
 };
 
 main();
